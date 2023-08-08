d1d5dee3e0a99c809bd90074688e42d9b3e35768
Kadavercian01 <indigo.gamer.life@gmail.com>
2023-08-08 19:52:44 +0300

    commit -5

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImp.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImp.java
@@ -2,6 +2,7 @@ package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.RoleDao;
 import ru.kata.spring.boot_security.demo.model.Role;
 
@@ -10,15 +11,18 @@ import java.util.List;
 public class RoleServiceImp implements  RoleService{
 
     private final RoleDao roleDao;
+
     @Autowired
     public RoleServiceImp(RoleDao roleDao) {
         this.roleDao = roleDao;
     }
+
     @Override
     public List<Role> getAllRoles() {
         return roleDao.getAllRoles();
     }
 
+    @Transactional
     @Override
     public void saveRole(Role role) {
         roleDao.saveRole(role);
